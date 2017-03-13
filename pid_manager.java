9be0ecabdb7fb5ccb60de6cda3241688e150dfe6
Dathan Huang <dh@schrius.com>
2017-03-13 09:10:43 -0400

    Add files via upload

--- pid_manager.java
+++ pid_manager.java
@@ -1,7 +1,9 @@
+import java.util.Random;
+
 /*Author: Dathan Huang 
  *CISC3320 Operating System
- * Date: March 4, 2017
-
+ * Date: March 12, 2017
+ *Assignment 2.
  *idSize is a constant variable holding the size of the pid_manager
  *idnum is a private variable that indicate current amount of pid that has created.
  * p_id[] is an array holding each pid element
@@ -48,4 +50,37 @@ public class pid_manager {
 			p_id[pid-MIN_PID] = 0;
 		idnum--;
 	}
+	// main method create 100 threads.
+	public static void main (String[] args){
+		int num_threads = 100;
+		pid_manager pid_test = new pid_manager();
+		System.out.println("Ready \n");
+		pid_test.allocate_map();
+		for(int i = 0; i < num_threads; i++){
+			Thread rc = new Thread(new mthread(pid_test));
+			rc.start();
+	}
 }
+}
+
+
+class mthread implements Runnable{
+	int mypid;
+	pid_manager pidmanager;
+	mthread(pid_manager pidm){
+		pidmanager = pidm;
+		mypid = pidm.allocate_pid();
+	}
+	public void run(){
+		try {
+			Random rand = new Random();
+			System.out.printf("# %d Thread starts.\n", mypid);
+			Thread.sleep(rand.nextInt(8000)+1000);
+			pidmanager.release_pid(mypid);
+			System.out.printf("Thread ID: %d complete. \n", mypid);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
